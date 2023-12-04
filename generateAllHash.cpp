93c7610b91af49f8d3ebceae76a8e45b317667a8
Zhengge Tang <zhengge.tang@duke.edu>
2023-12-03 22:16:32 -0500

    recursive find file option

--- generateAllHash.cpp
+++ generateAllHash.cpp
@@ -3,17 +3,16 @@
 // g++ -o generateAllHash generateAllHash.cpp -lssl -lcrypto
 
 int main() {
-  std::vector<std::string> pathes = {
-      "./example/folder1", "./example/folder2", "./example/folder3", "./example/folder4"};
+  std::vector<std::string> pathes = {"./example"};
 
   for (auto path : pathes) {
     std::string pt = path + "/hashes.txt";
     remove(pt.c_str());
-    std::vector<std::string> hashes = allFiles(path);
+    std::vector<std::string> hashes = allFiles(path, true);
     FILE * fp = fopen(pt.c_str(), "wb");
 
     for (auto p : hashes) {
-      fprintf(fp, "%s:", p.c_str());
+      fprintf(fp, "%s:", getFileNameFromPath(p).c_str());
       std::string hash = getFileHash(p);
       fprintf(fp, "%s\n", hash.c_str());
     }
--- hash.hpp
+++ hash.hpp
@@ -21,9 +21,10 @@ void digest_message(const unsigned char * message,
 std::string getFileHash(const std::string & filename);
 std::string getVectorCharHash(const std::vector<char> & content);
 size_t getFileLength(std::string & filename);
-std::vector<std::string> allFiles(const std::string & directory);
+std::vector<std::string> allFiles(const std::string & directory, bool recursive);
 bool matchHash(std::string & hash, std::string & filename);
 bool checkFileExist(std::string & hash, std::string & directory);
 std::string findFileName(std::string & hash, std::string & directory);
 bool checkValidSHA256(std::string & hash);
-ContentMeta genContentMeta(std::string & hash, std::string & dir);
+
+std::string getFileNameFromPath(std::string & path);
